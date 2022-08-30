bf3d2690aaa306962d8f14062e0b4ff8ed405830
Bill Wang <ozbillwang@users.noreply.github.com>
2022-08-30 16:47:01 +1000

    feature/#49 (#50)
    Co-authored-by: Bill Wang <bill.wang>

--- build.sh
+++ build.sh
@@ -17,15 +17,23 @@ DOCKER_PUSH="docker buildx build --no-cache --push --platform linux/amd64,linux/
 VERSION=($(docker run -i --rm ${image}:latest version|awk '{print $NF}'))
 echo ${VERSION}
 
+# install crane
+curl -L https://github.com/tsuru/crane/releases/download/1.0.0/crane-1.0.0-linux_amd64.tar.gz -o crane-1.0.0-linux_amd64.tar.gz
+tar zxvf crane-1.0.0-linux_amd64.tar.gz
+chmod +x crane
+
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == false ]]; then
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   docker buildx create --use
-  ${DOCKER_PUSH} -t ${image}:latest .
   ${DOCKER_PUSH} -t ${image}:v${VERSION} .
+  crane copy ${image}:v${VERSION} ${image}:latest
+  crane copy ${image}:v${VERSION} ${image}:${VERSION}
+
 fi
 
 if [[ "$TRAVIS_BRANCH" == "feature/non-root" && "$TRAVIS_PULL_REQUEST" == false ]]; then
   docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
   ${DOCKER_PUSH} -t ${image}:user .
-  ${DOCKER_PUSH} -t ${image}:v${VERSION}-user .
+  crane copy ${image}:user ${image}:v${VERSION}-user
+  crane copy ${image}:user ${image}:${VERSION}-user
 fi
